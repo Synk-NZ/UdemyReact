c00418d62895c27eb02c784ea82d10060aa076b3
Karl <Karl@phoenixdown.co.nz>
2018-12-05 17:57:06 +1300

    Added a javascript text object and called it using react. End of Secton 2

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a react component 
-const App = function() {
+const App = () => {
+    const buttonText = { text: 'Click me'};
+    const labelText = 'Enter name: ';
 return (
     <div>
-        <label class="label" for="name">Ehter Name: </label>
+        <label className="label" htmlFor="name">
+            {labelText}
+        </label>
         <input id="name" type="text"/>
-        <button style ={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+        <button style ={{backgroundColor: 'blue', color: 'white'}}>
+            {buttonText.text}
+        </button>
     </div>
     );
 };
